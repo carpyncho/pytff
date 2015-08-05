aaf54b915d6761b4951c85336d311c6aa8782229
Juan B Cabral <jbc.develop@gmail.com>
2015-08-05 01:17:11 -0300

    version 0.6

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ from setuptools import setup, find_packages
 # CONSTANTS
 #==============================================================================
 
-VERSION = ('0', '5', '1')
+VERSION = ('0', '6')
 
 STR_VERSION = ".".join(VERSION)
 
